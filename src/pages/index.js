f08b71e999b0537b7bfcc2b26287ebd9cc11937f
littledazed <ogum536@gmailcom>
2020-03-31 17:39:43 -0500

    react-grid-system and css working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
 import React from "react"
+
+import { Container, Row, Col } from "react-grid-system"
 import { Link } from "gatsby"
 
 require("typeface-karla")
 
 export default () => (
-    <div style={{ color: `purple`, fontFamily: `Karla` }}>
+  <div className="pink-div" style={{ fontFamily: `Karla` }}>
+    <Container>
       <Link to="/contact/">Contact</Link>
-      <p>What a world.</p>
-      
-    </div>
-  )
+      <Row>
+        <Col sm={4}>One of three columns</Col>
+        <Col sm={4}>One of three columns</Col>
+        <Col sm={4}>One of three columns</Col>
+      </Row>
+    </Container>
+  </div>
+)
