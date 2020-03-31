6e72100d73f860f0b80de6dbc1c39e4a60b198a2
littledazed <ogum536@gmailcom>
2020-03-31 17:10:12 -0500

    change content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+require("typeface-karla")
+
+export default () => (
+    <div style={{ color: `purple`, fontFamily: `Karla` }}>
+      <Link to="/contact/">Contact</Link>
+      <p>What a world.</p>
+      
+    </div>
+  )
