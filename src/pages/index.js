91a6bb1fc9d660ddf0b7b5d675a12cddc37233b5
littledazed <ogum536@gmailcom>
2020-03-31 19:28:23 -0500

    trying mdbreact

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,81 +1,20 @@
 import React from "react"
-import TitleHeader from "../components/titleheader"
-
 import { Link } from "gatsby"
 
-import { Container, Row, Col } from "react-grid-system"
-import { ScreenClassRender } from "react-grid-system"
-import { Visible } from "react-grid-system"
+import "bootstrap-css-only/css/bootstrap.min.css"
+// import "mdbreact/dist/css/mdb.css"
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
+import TitleHeader from "../components/titleheader"
 
 require("typeface-karla")
 require("typeface-poppins")
 
 export default () => (
-  <div
-    className="pink-div"
-    style={{ fontFamily: `Poppins`, fontWeight: `300` }}
-  >
-    <Container fluid>
-      <Link to="/contact/">blah</Link>
-      <Row>
-        <Col md={1} debug>
-          md=1
-        </Col>
-        <Col md={1} debug>
-          md=1
-        </Col>
-        <Col md={1} debug>
-          md=1
-        </Col>
-        <Col md={1} debug>
-          md=1
-        </Col>
-        <Col md={1} debug>
-          md=1
-        </Col>
-        <Col md={1} debug>
-          md=1
-        </Col>
-        <Col md={1} debug>
-          md=1
-        </Col>
-        <Col md={1} debug>
-          md=1
-        </Col>
-        <Col md={1} debug>
-          md=1
-        </Col>
-        <Col md={1} debug>
-          md=1
-        </Col>
-        <Col md={1} debug>
-          md=1
-        </Col>
-        <Col md={1} debug>
-          md=1
-        </Col>
-      </Row>
-      <TitleHeader />
-
-      <p>
-        <span>Your current screen class is </span>
-        <Visible xs>
-          <strong>xs</strong>
-        </Visible>
-        <Visible sm>
-          <strong>sm</strong>
-        </Visible>
-        <Visible md>
-          <strong>md</strong>
-        </Visible>
-        <Visible lg>
-          <strong>lg</strong>
-        </Visible>
-        <Visible xl>
-          <strong>xl</strong>
-        </Visible>
-        <span>.</span>
-      </p>
-    </Container>
-  </div>
+  <MDBContainer>
+    <MDBRow>
+      <MDBCol md="4">.col-md-4</MDBCol>
+      <MDBCol md="4">.col-md-4</MDBCol>
+      <MDBCol md="4">.col-md-4</MDBCol>
+    </MDBRow>
+  </MDBContainer>
 )
