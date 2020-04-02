25869e8e38227d574b23918db4fe941dff26efa5
littledazed <ogum536@gmailcom>
2020-04-01 21:11:34 -0500

    wrapped index with layouts and redid password page

---
+++ src/layouts/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default ({ children }) => (
+  <div className="container-fluid px-0">{children}</div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../layouts/index"
 
 // import Navbar from "../components/navbar.js"
 import Footer from "../components/footer"
@@ -8,7 +9,7 @@ require("typeface-karla")
 require("typeface-poppins")
 
 export default () => (
-  <div className="container-fluid px-0">
+  <Layout>
     <Link to="/">Home</Link>
     <Link to="/proj-etc/">ETC</Link>
     <Link to="/resume/">Resume</Link>
@@ -20,5 +21,5 @@ export default () => (
       </div>
     </div>
     <Footer />
-  </div>
+  </Layout>
 )
